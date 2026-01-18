ef5a92965897f3008f8dc81353ced3b12ba34d3e
LENOVO <LENOVO@DESKTOP-G9D6BGV>
2026-01-18 00:13:58 -0800

    Add project files.

---
+++ ecommerce/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ecommerce.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Customer_Id { get; set; }
+        public string Customer_Name { get; set; }
+    }
+}
---
+++ ecommerce/Models/MyContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce.Models
+{
+
+    public class MyContext : DbContext
+    {
+
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Admin> tbl_admin { get; set; }
+    }
+}
